7b25692d03a1758812b7a23dac8f0fff8c9af047
Vishal Patel <vkpatel924@gmail.com>
2025-03-20 01:05:53 +0530

    Introducing immutability to IPP-USB OCI image

--- rock/scripts/run-ipp-usb.sh
+++ rock/scripts/run-ipp-usb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#set -e -x
+set -eux
 
 # Create needed directories (ignore errors)
 mkdir -p /etc/ipp-usb || :
@@ -14,6 +14,19 @@ yes no | cp -i /usr/share/ipp-usb/quirks/* /etc/ipp-usb/quirks >/dev/null 2>&1 |
 if [ ! -f /etc/ipp-usb/ipp-usb.conf ]; then
     cp /usr/share/ipp-usb/ipp-usb.conf /etc/ipp-usb/ >/dev/null 2>&1 || :
 fi
+
+# Wait for avahi-daemon to initialize
+while true; do
+    if [ -f "/var/run/avahi-daemon/pid" ] || [ -f "/run/avahi-daemon/pid" ]; then
+        echo "[$(date)] avahi-daemon is active. Starting ipp-usb..."
+        break
+    fi
+    echo "[$(date)] Waiting for avahi-daemon to initialize..."
+    sleep 1
+done
+
+# Run ipp-usb with logging
+echo "[$(date)] Running ipp-usb..."
 
 # Run ipp-usb with the provided command-line arguments
 exec /usr/sbin/ipp-usb "$@"

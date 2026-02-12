986cfc2a2467e8d0c5a4be8e66b3a162ad41c4d2
Vishal Patel <127092820+im0vishal@users.noreply.github.com>
2026-02-12 18:30:04 +0530

    Introduces immutability to IPP-USB OCI image (#106)
    * Introduce immutability to IPP-USB OCI image
    * Update README to include runtime flags for data persistence
    * Update documentation for configuration and logging setup
    * Include nano in Rock image and improve docs with persistent storage for ipp-usb
    * Enhance persistence: Separate volumes for state, config, and logs in ipp-usb.

--- rock/scripts/run-ipp-usb.sh
+++ rock/scripts/run-ipp-usb.sh
@@ -1,19 +1,32 @@
 #!/bin/sh
+set -eux
 
-#set -e -x
-
-# Create needed directories (ignore errors)
+# Create necessary directories
 mkdir -p /etc/ipp-usb || :
+mkdir -p /etc/ipp-usb/quirks || :
+mkdir -p /var/ipp-usb/lock || :
+mkdir -p /var/ipp-usb/dev || :
 mkdir -p /var/log/ipp-usb || :
-mkdir -p /var/lock || :
-mkdir -p /var/dev || :
-mkdir -p /usr/share/ipp-usb/quirks || :
 
+# Copy quirks files if not present
+cp -rn /usr/share/ipp-usb/quirks/* /etc/ipp-usb/quirks/ >/dev/null 2>&1 || :
 # Put config files in place (do not overwrite existing user config)
-yes no | cp -i /usr/share/ipp-usb/quirks/* /etc/ipp-usb/quirks >/dev/null 2>&1 || :
 if [ ! -f /etc/ipp-usb/ipp-usb.conf ]; then
-    cp /usr/share/ipp-usb/ipp-usb.conf /etc/ipp-usb/ >/dev/null 2>&1 || :
+    cp /etc/ipp-usb.conf /etc/ipp-usb/ >/dev/null 2>&1
 fi
+
+# Wait for avahi-daemon
+while true; do
+    if [ -f "/var/run/avahi-daemon/pid" ] || [ -f "/run/avahi-daemon/pid" ]; then
+        echo "[$(date)] avahi-daemon is active. Starting ipp-usb..."
+        break
+    fi
+    echo "[$(date)] Waiting for avahi-daemon to initialize..."
+    sleep 1
+done
+
+# Run ipp-usb with logging
+echo "[$(date)] Running ipp-usb..."
 
 # Run ipp-usb with the provided command-line arguments
 exec /usr/sbin/ipp-usb "$@"
